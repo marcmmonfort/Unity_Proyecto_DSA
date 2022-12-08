e1c5fb266d7a98d7bade9dc18476be0a77d1fe96
Marc <marc.moran@estudiantat.upc.edu>
2022-12-08 01:01:09 +0100

    Últimos retoques y listo para pasarlo a Android.

--- Assets/_Complete-Game/Scripts/Player.cs
+++ Assets/_Complete-Game/Scripts/Player.cs
@@ -49,51 +49,67 @@ public class Player : MovingObject
         int horizontal = 0;
         int vertical = 0;
 
-#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+        #if UNITY_STANDALONE || UNITY_WEBPLAYER // Check if we are running either in the Unity editor or in a standalone build.
 
-        horizontal = (int)Input.GetAxisRaw("Horizontal");
-        vertical = (int)Input.GetAxisRaw("Vertical");
+            //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+            horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
 
-        // Verificamos si nos estamos moviendo horizontalmente ...
-        if (horizontal != 0)
-        {
-            vertical = 0; // Para que el jugador no se mueva en diagonal.
-        }
+            //Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+            vertical = (int) (Input.GetAxisRaw ("Vertical"));
 
-#else
-
-        if (Input.touchCount > 0)
-        {
-            touchOrigin myTouch = Input.touches[0];
-
-            if (myTouch.phase == TouchPhase.Began)
+            //Check if moving horizontally, if so set vertical to zero.
+            if(horizontal != 0)
             {
-                touchOrigin = myTouch.position;
+                vertical = 0;
+            }
+            
+        
+        #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+
+            //Check if Input has registered more than zero touches
+            if (Input.touchCount > 0)
+            {
+                //Store the first touch detected.
+                Touch myTouch = Input.touches[0];
+
+                //Check if the phase of that touch equals Began
+                if (myTouch.phase == TouchPhase.Began)
+                {
+                    //If so, set touchOrigin to the position of that touch
+                    touchOrigin = myTouch.position;
+                }
+
+                //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+                else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+                {
+                    //Set touchEnd to equal the position of this touch
+                    Vector2 touchEnd = myTouch.position;
+
+                    //Calculate the difference between the beginning and end of the touch on the x axis.
+                    float x = touchEnd.x - touchOrigin.x;
+
+                    //Calculate the difference between the beginning and end of the touch on the y axis.
+                    float y = touchEnd.y - touchOrigin.y;
+
+                    //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                    touchOrigin.x = -1;
+
+                    //Check if the difference along the x axis is greater than the difference along the y axis.
+                    if (Mathf.Abs(x) > Mathf.Abs(y))
+                        //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                        horizontal = x > 0 ? 1 : -1;
+                    else
+                        //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                        vertical = y > 0 ? 1 : -1;
+                }
             }
 
-            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-            {
-                Vector2 touchEnd = myTouch.position;
-                float x = touchEnd.x - touchOrigin.x;
-                float y = touchEnd.y - touchOrigin.y;
-                touchOrigin.x = -1;
-
-                // Tenemos que saber si el deslizamiento del dedo ha sido más horizontal o más vertical ...
-                if (Mathf.Abs(x) > Mathf.Abs(y))
-                {
-                    horizontal = x > 0 ? 1 : -1;
-                }
-                else
-                {
-                    vertical = y > 0 ? 1 : -1;
-                }
-            }
-        }
-
-# endif
-
+        #endif // End of mobile platform dependendent compilation section started above with #elif.
+        
         if (horizontal != 0 || vertical != 0)
         {
+            //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+            //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
             AttemptMove<Wall>(horizontal, vertical);
         }
     }
